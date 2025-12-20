7fb55c22f339fc83a8d98a1e7c63ec903a7fc69e
intellectro <oluebubeaneke247@gmail.com>
2025-12-20 05:08:28 -0800

    feat: Initialize project with React Router, TanStack Query, ghost user authentication, and core UI components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./route/route";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const App = () => <RouterProvider router={router} />;
+const queryClient = new QueryClient();
+
+const App = () => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    )
+}
 
 export default App;
--- src/salesive-api-axios.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "salesive-api-axios";
